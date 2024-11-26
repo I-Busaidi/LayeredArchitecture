e90df0517ed132579ba49444a28a9799bb0ab42b
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-26 09:06:22 +0400

    added ITransactionService interface and TransactionService class methods.

--- Services/ITransactionService.cs
+++ Services/ITransactionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LayeredArchitecture.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace LayeredArchitecture.Services
 {
     public interface ITransactionService
     {
+        public void AddTransaction(Transaction transaction);
 
+        public void UpdaterTransaction(Transaction transaction);
+
+        public void DeleteTransaction(Transaction transaction);
+
+        public IEnumerable<Transaction> GetAllTransactions();
+
+        public IEnumerable<Transaction> GetTransactionsBySourceAccountNum(int sourceAccNum);
+
+        public IEnumerable<Transaction> GetTransactionsByDestinationAccountNum(int destAccNum);
+
+        public IEnumerable<Transaction> GetTransactionsByDateRange(DateTime fromDate, DateTime toDate);
+
+        public IEnumerable<Transaction> GetAccountTransactionsByDateRange(int accNum, DateTime fromDate, DateTime toDate);
     }
 }
--- Services/TransactionService.cs
+++ Services/TransactionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LayeredArchitecture.Models;
+using LayeredArchitecture.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,52 @@ using System.Threading.Tasks;
 
 namespace LayeredArchitecture.Services
 {
-    public class TransactionService
+    public class TransactionService : ITransactionService
     {
+        private readonly ITransactionRepository _transactionRepository;
+        public TransactionService(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            _transactionRepository.AddTransaction(transaction);
+        }
+
+        public void UpdaterTransaction(Transaction transaction)
+        {
+            _transactionRepository.UpdateTransaction(transaction);
+        }
+
+        public void DeleteTransaction(Transaction transaction)
+        {
+            _transactionRepository.DeleteTransaction(transaction.sourceAccNum, transaction.transactionId);
+        }
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return _transactionRepository.GetAllTransactions();
+        }
+
+        public IEnumerable<Transaction> GetTransactionsBySourceAccountNum(int sourceAccNum)
+        {
+            return _transactionRepository.GetTransactionsBySourceAccountNum(sourceAccNum);
+        }
+
+        public IEnumerable<Transaction> GetTransactionsByDestinationAccountNum(int destAccNum)
+        {
+            return _transactionRepository.GetTransactionsByDestinationAccountNum(destAccNum);
+        }
+
+        public IEnumerable<Transaction> GetTransactionsByDateRange(DateTime fromDate, DateTime toDate)
+        {
+            return _transactionRepository.GetTransactionsByDateRange(fromDate, toDate);
+        }
+
+        public IEnumerable<Transaction> GetAccountTransactionsByDateRange(int accNum, DateTime fromDate, DateTime toDate)
+        {
+            return _transactionRepository.GetAccountTransactionsByDateRange(accNum, fromDate, toDate);
+        }
     }
 }
